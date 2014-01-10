036338e78d3892195dc0efa272134584f61c1d8e
Geir Morten <gm.larsen@oyvang.org>
2014-01-10 14:29:02 +0100

    Bruker og DatabaseConnector :  add/delete bruker

--- src/main/java/no/hist/tdat/database/DatabaseConnector.java
+++ src/main/java/no/hist/tdat/database/DatabaseConnector.java
@@ -2,10 +2,9 @@ package no.hist.tdat.database;
 
 import no.hist.tdat.javabeans.Bruker;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * DatabaseConnector kobler til databasen og gjør spørring, for deretter å stenge tilkoblingen.
@@ -29,6 +28,7 @@ public class DatabaseConnector {
         }
     }
 //EKSEMPEL PÅ HVORDAN STRUKTUREN SKAL VÆRE PÅ QUERIES
+
     public boolean leggTilBruker(Bruker bruker){
         if(bruker == null){
             return false;
@@ -39,11 +39,11 @@ public class DatabaseConnector {
             try(PreparedStatement prepStat = con.prepareStatement(query)){
                 con.setAutoCommit(false);
                     prepStat.setString(1, bruker.getMail());
-                    prepStat.setString(2, bruker.getRettighet());
+                    prepStat.setInt(2, bruker.getRettighet());
                     prepStat.setString(3, bruker.getFornavn());
                     prepStat.setString(4, bruker.getEtternavn());
                     prepStat.setString(5, bruker.genererPassord());
-                    prepStat.setInt(6, ACTIVE);
+                    prepStat.setInt(6, bruker.getAktiv());
                     prepStat.executeUpdate();
                 con.commit();
                 con.setAutoCommit(true);
@@ -60,21 +60,29 @@ public class DatabaseConnector {
         }
     }
 
-    public boolean finnBruker(Bruker bruker){
+    /**
+     *Oppdatterer en spesifikk bruker
+     * @param bruker Den brukeren du il endre
+     * @param mail mailen til den du skal endre, denne er i tilfelle man endrer mail
+     * @return true om bruker blir oppdatert ellers false
+     */
+    public boolean oppdaterBruker(Bruker bruker, String mail){
         if(bruker == null){
             return false;
         }
         try(Connection con = DriverManager.getConnection(url, username, password)){
-            String query = "INSERT INTO brukere (mail, rettighet_id, fornavn, etternavn, passord, aktiv)"
-                    +"VALUES (?,?,?, ?,?,?)";
+            String query = "UPDATE brukere SET mail = ?, rettighet_id = ?, fornavn = ?, etternavn = ?, passord = ?, aktiv = ?" +
+                                "WHERE mail = ?";
+
             try(PreparedStatement prepStat = con.prepareStatement(query)){
                 con.setAutoCommit(false);
                 prepStat.setString(1, bruker.getMail());
-                prepStat.setString(2, bruker.getRettighet());
+                prepStat.setInt(2, bruker.getRettighet());
                 prepStat.setString(3, bruker.getFornavn());
                 prepStat.setString(4, bruker.getEtternavn());
                 prepStat.setString(5, bruker.genererPassord());
-                prepStat.setInt(6, ACTIVE);
+                prepStat.setInt(6, bruker.getAktiv());
+                prepStat.setString(7, mail);
                 prepStat.executeUpdate();
                 con.commit();
                 con.setAutoCommit(true);
@@ -88,6 +96,46 @@ public class DatabaseConnector {
             e.printStackTrace();
             System.out.println(CONNECTION_ERROR);
             return false;
+        }
+    }
+
+    /**
+     * Tar inn en string som søkeord, søker i databasen etter mail, fornavn, etternavn som ligner på søkeordet.
+     * @param soeketekst Søkeord etter bruker
+     * @return ArrayList med bruker objekter eller null om ingen finnes.
+     * @author GM
+     */
+    public ArrayList<Bruker> finnBruker(String soeketekst){
+        if(soeketekst == null){
+            return null;
+        }
+        try(Connection con = DriverManager.getConnection(url, username, password)){
+            String query = "SELECT * FROM brukere WHERE mail LIKE ? OR fornavn LIKE ? OR etternavn LIKE ?";
+            try(PreparedStatement prepStat = con.prepareStatement(query)){
+                con.setAutoCommit(false);
+                prepStat.setString(1, soeketekst);
+                prepStat.setString(2, soeketekst);
+                prepStat.setString(3, soeketekst);
+                ResultSet rs = prepStat.executeQuery();
+                con.commit();
+                con.setAutoCommit(true);
+
+                ArrayList<Bruker> brukere = new ArrayList<Bruker>();
+                while(rs.next()){
+                    Bruker objekt = new Bruker(rs.getString("mail"), rs.getInt("rettighet"), rs.getString("fornavn"), rs.getString("etternvn"), rs.getInt("aktiv"));
+                    brukere.add(objekt);
+                }
+                return brukere;
+
+            }catch (SQLException e){
+                e.printStackTrace();
+                System.out.println(QUERY_ERROR);
+                return null;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(CONNECTION_ERROR);
+            return null;
         }
     }
 
--- src/main/java/no/hist/tdat/javabeans/Bruker.java
+++ src/main/java/no/hist/tdat/javabeans/Bruker.java
@@ -2,27 +2,39 @@ package no.hist.tdat.javabeans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
- * Created by oyvang on 09.01.14.
+ * Created by vimCnett on 09.01.14.
  */
 public class Bruker {
-
+    private static final String RANDOM_TEGN  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+    private final Random random = new Random();
     private String mail;
-    private String rettighet;
+    private Integer rettighet;
     private String fornavn;
     private String etternavn;
     private String passord;
     private int aktiv;
     private ArrayList<Emner> emner;
 
-    public Bruker(String mail, String rettighet, String fornavn, String etternavn, int aktiv){
+    public Bruker(String mail, Integer rettighet, String fornavn, String etternavn, int aktiv){
         this.mail = mail;
         this.rettighet = rettighet;
         this. fornavn = fornavn;
         this.etternavn = etternavn;
         this.passord = genererPassord();
         this.aktiv = aktiv;
+        emner = new ArrayList<Emner>();
+    }
+
+    public Bruker(String mail, Integer rettighet, String fornavn, String etternavn){
+        this.mail = mail;
+        this.rettighet = rettighet;
+        this. fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.passord = genererPassord();
+        this.aktiv = 1;
         emner = new ArrayList<Emner>();
     }
 
@@ -34,11 +46,11 @@ public class Bruker {
         this.mail = mail;
     }
 
-    public String getRettighet() {
+    public Integer getRettighet() {
         return rettighet;
     }
 
-    public void setRettighet(String rettighet) {
+    public void setRettighet(Integer rettighet) {
         this.rettighet = rettighet;
     }
 
@@ -62,8 +74,12 @@ public class Bruker {
         return passord;
     }
 
+    /**
+     * Bruker hjelpemetoden krypterPassord til å sette passord til bruker
+     * @param passord
+     */
     public void setPassord(String passord) {
-        this.passord = passord;
+        this.passord = krypterPassord(passord);
     }
 
     public int getAktiv() {
@@ -82,18 +98,34 @@ public class Bruker {
         this.emner = emner;
     }
 
-    public String genererPassord(){
-        return "hello,world"; //todo legg til random kode
+    /**
+     * Hjelpemetode til genererPassord, generer random int verdier
+     * @return et tall mellom 0 og RANDOM_TEGN.length()
+     */
+    public int randomIndex (){
+        int min = 0;
+        int max = RANDOM_TEGN.length();
+        return random.nextInt((max-min)+1)+min;
+
     }
 
     /**
-     * Legger til emne(String verdi) en bruker er medlem av, denne listen inneholder ikke brukerrettigheter innen et emne eller emne objekt.
-     * Det er gitt at emnekode og emnenavn finnes fra før i databasen.
-     * @param emnekode
-     * @param emnenavn
+     * Generer et random passord på lengde 6 tegn
+     * @return kryptert random passord på lengde 6
      */
-    public void addEmne(String emnekode, String emnenavn){
-        //emner.add(emnekode+", "+ emnenavn);
+    public String genererPassord(){
+        String passord = "";
+        for (int i = 0; i <6 ; i++) {
+            passord+=RANDOM_TEGN.charAt(randomIndex());
+        }
+        return passord;
+    }
+
+
+
+
+    public void addEmne(){
+        //TODO legg til et emne en bruker er medlem av. Her skal ikke tilgangsrettigheter være
     }
 
 }
