22c34af999a411e6a42a2faf076bed9e1df57049
Ted <tedjohan@gmail.com>
2014-01-10 15:03:55 +0100

    passordkryptering, konstruktører til bruker (tom og med mail, passord, startet opp med loginkontroller

--- src/main/java/no/hist/tdat/database/DatabaseConnector.java
+++ src/main/java/no/hist/tdat/database/DatabaseConnector.java
@@ -2,10 +2,7 @@ package no.hist.tdat.database;
 
 import no.hist.tdat.javabeans.Bruker;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * DatabaseConnector kobler til databasen og gjør spørring, for deretter å stenge tilkoblingen.
@@ -59,4 +56,49 @@ public class DatabaseConnector {
             return false;
         }
     }
+
+    /**
+     * Tar inn et brukerobjekt, henter brukerinfo fra databasen, sjekker om passord er rett.
+     * @param bruker brukerobjekt med passord innskrevet av bruker, samt brukernavn ( email )
+     * @return boolean, true om passord og brukernavn stemmer overens.
+     * @see "The Java Programming Language"
+     * @throws SQLException  om tilkoblingsproblemer til database
+     * @throws SQLException  om feil i spørringen
+     * @author	vimCnett
+     */
+    public boolean loggInn(Bruker bruker){
+        if(bruker == null){
+            return false;
+        }
+        try(Connection con = DriverManager.getConnection(url, username, password)){
+            String query = "SELECT (mail, rettighet_id, fornavn, etternavn, passord, aktiv) FROM brukere"
+                    +"WHERE (mail =?,passord =?)";
+            try(PreparedStatement prepStat = con.prepareStatement(query)){
+                con.setAutoCommit(false);
+                prepStat.setString(1, bruker.getMail());
+                prepStat.setString(2, bruker.getPassord());
+               try( ResultSet rs = prepStat.executeQuery()){
+                if(krypterPassord(bruker.getPassord()).equals()){}
+
+               }catch (SQLException e){
+                   e.printStackTrace();
+                   System.out.println(QUERY_ERROR);
+                   return false;
+               }
+                con.commit();
+                con.setAutoCommit(true);
+                return true;
+            }catch (SQLException e){
+                e.printStackTrace();
+                System.out.println(QUERY_ERROR);
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(CONNECTION_ERROR);
+            return false;
+        }
+    }
+
+
 }
--- src/main/java/no/hist/tdat/javabeans/Bruker.java
+++ src/main/java/no/hist/tdat/javabeans/Bruker.java
@@ -26,6 +26,18 @@ public class Bruker {
         emner = new ArrayList<Emner>();
     }
 
+    public Bruker(){}
+
+    /**
+     * Konstruktør for innlogging
+     * @param mail
+     * @param passord
+     */
+    public Bruker(String mail, String passord){
+        this.mail = mail;
+        this.passord = krypterPassord(passord);
+    }
+
     public String getMail() {
         return mail;
     }
@@ -96,4 +108,38 @@ public class Bruker {
         //emner.add(emnekode+", "+ emnenavn);
     }
 
+    /**
+     *Tar inn en string fra brukeren og krypterer passordet.
+     * @param pw passord skrevet inn av bruker
+     * @see "The Java Programming Language"
+     * @return String som kryptert passord
+     * @author vimCnett
+     */
+    static String krypterPassord(String pw){
+        String alphaString = ("abcdefghijklmnopqrstuvwxyz").toUpperCase();
+        char[] alphabet = ("abcdefghijklmnopqrstuvwxyz"+alphaString+"123567890").toCharArray();
+        int length = alphabet.length;
+        String kryptertPassord ="";
+        int verdi = 0;
+        int verdi2 = 0;
+        Character part1;
+        char part2;
+        char part3;
+        char part4;
+        for (int i = 0; i <pw.length() ; i++) {
+            verdi = (int)pw.charAt(i);
+            verdi*=verdi+length/2;
+            verdi2=i*verdi*verdi2;
+            part1= (char)alphabet[(verdi%length)];
+            part2= (char)alphabet[((i*103)%length)];
+            part3= (char)alphabet[(int)((i*verdi2*708)%length)];
+            part4= (char)alphabet[(((1337*verdi2+verdi)%713)%length)];
+
+            kryptertPassord+=part1;
+            kryptertPassord+=part2;
+            kryptertPassord+=part3;
+            kryptertPassord+=part4;
+        }
+        return kryptertPassord;
+    }
 }
