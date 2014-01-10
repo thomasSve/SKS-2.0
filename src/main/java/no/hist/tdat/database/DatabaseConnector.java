6b6e70f702192567dc8f6f626fb97a3e4927a205
Jørgen Lien Sellæg <jorgen@guut.org>
2014-01-10 14:52:57 +0100

    Spring datasource

--- src/main/java/no/hist/tdat/database/DatabaseConnector.java
+++ src/main/java/no/hist/tdat/database/DatabaseConnector.java
@@ -1,7 +1,11 @@
 package no.hist.tdat.database;
 
 import no.hist.tdat.javabeans.Bruker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,51 +16,25 @@ import java.sql.SQLException;
  *
  * @author VimCnett
  */
+@Component
 public class DatabaseConnector {
     private static final String QUERY_ERROR = "FEIL I SPØRRING";
     private static final String CONNECTION_ERROR = "FEIL VED TILKOBLING TIL DATABASE";
     private static final Integer ACTIVE = 1;
-    private final String username = "root";
-    private final String password = "";
-    private final String url = "jdbc:mysql://localhost:3306/sks";
+    private final String leggTilBrukerSQL = "INSERT INTO brukere (mail, rettighet_id, fornavn, etternavn, passord, aktiv) VALUES (?,?,?,?,?,?)";
 
-    public DatabaseConnector() {
-        try {
-            String driver = "com.mysql.jdbc.Driver";
-            Class.forName(driver);
-        } catch (ClassNotFoundException e) {
-            System.out.println(e + " Du har feil navn på databasedriveren.");
-        }
-    }
-//EKSEMPEL PÅ HVORDAN STRUKTUREN SKAL VÆRE PÅ QUERIES
-    public boolean leggTilBruker(Bruker bruker){
-        if(bruker == null){
-            return false;
-        }
-        try(Connection con = DriverManager.getConnection(url, username, password)){
-            String query = "INSERT INTO brukere (mail, rettighet_id, fornavn, etternavn, passord, aktiv)"
-                                            +"VALUES (?,?,?, ?,?,?)";
-            try(PreparedStatement prepStat = con.prepareStatement(query)){
-                con.setAutoCommit(false);
-                    prepStat.setString(1, bruker.getMail());
-                    prepStat.setString(2, bruker.getRettighet());
-                    prepStat.setString(3, bruker.getFornavn());
-                    prepStat.setString(4, bruker.getEtternavn());
-                    prepStat.setString(5, bruker.genererPassord());
-                    prepStat.setInt(6, ACTIVE);
-                    prepStat.executeUpdate();
-                con.commit();
-                con.setAutoCommit(true);
-                return true;
-            }catch (SQLException e){
-                e.printStackTrace();
-                System.out.println(QUERY_ERROR);
-                return false;
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-            System.out.println(CONNECTION_ERROR);
-            return false;
-        }
+    @Autowired
+    DataSource dataKilde;
+
+    public void leggTilBruker(Bruker bruker){
+        JdbcTemplate con = new JdbcTemplate(dataKilde);
+        con.update(leggTilBrukerSQL,
+                                    bruker.getMail(),
+                                    bruker.getRettighet(),
+                                    bruker.getFornavn(),
+                                    bruker.getFornavn(),
+                                    bruker.getEtternavn(),
+                                    bruker.genererPassord(),
+                                    ACTIVE);
     }
 }
