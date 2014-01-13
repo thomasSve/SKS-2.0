1744d56e110203a630f3a8705e548375055afcd0
Jørgen Lien Sellæg <jorgen@guut.org>
2014-01-13 09:17:27 +0100

    Rm datasources and fix stuff in databaseconnector

--- src/main/java/no/hist/tdat/database/DatabaseConnector.java
+++ src/main/java/no/hist/tdat/database/DatabaseConnector.java
@@ -1,15 +1,14 @@
 package no.hist.tdat.database;
 
+import no.hist.tdat.database.verktoy.BrukerKoordinerer;
 import no.hist.tdat.javabeans.Bruker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 
 /**
  * DatabaseConnector kobler til databasen og gjør spørring, for deretter å stenge tilkoblingen.
@@ -21,12 +20,16 @@ public class DatabaseConnector {
     private static final String QUERY_ERROR = "FEIL I SPØRRING";
     private static final String CONNECTION_ERROR = "FEIL VED TILKOBLING TIL DATABASE";
     private static final Integer ACTIVE = 1;
+
+    // **** Legger alle Queryes her. Ikke fordi vi må, men fordi Grethe liker det sånn... //TODO remove this
+
     private final String leggTilBrukerSQL = "INSERT INTO brukere (mail, rettighet_id, fornavn, etternavn, passord, aktiv) VALUES (?,?,?,?,?,?)";
     private final String oppdaterBrukerSQL = "UPDATE brukere SET mail = ?, rettighet_id = ?, fornavn = ?, etternavn = ?, passord = ?, aktiv = ? WHERE mail = ?";
     private final String finnBrukerSQL = "SELECT * FROM brukere WHERE mail LIKE ? OR fornavn LIKE ? OR etternavn LIKE ?";
+    private final String slettBrukerSQL = "DELETE FROM brukere WHERE mail = ?";
 
     @Autowired
-    DataSource dataKilde;
+    DataSource dataKilde; //Felles datakilde for alle spørringer.
 
     public void leggTilBruker(Bruker bruker) {
         JdbcTemplate con = new JdbcTemplate(dataKilde);
@@ -60,5 +63,41 @@ public class DatabaseConnector {
                     ACTIVE);
             return true;
         }
+    }
+
+    /**
+     * Tar inn en string som søkeord, søker i databasen etter mail, fornavn, etternavn som ligner på søkeordet.
+     *
+     * @param soeketekst Søkeord etter bruker
+     * @return ArrayList med bruker objekter eller null om ingen finnes.
+     */
+    public ArrayList<Bruker> finnBruker(String soeketekst) {
+        if (soeketekst == null) {
+            return null;
+        }
+        JdbcTemplate con = new JdbcTemplate(dataKilde);
+        List<Bruker> brukerList = con.query(finnBrukerSQL, new BrukerKoordinerer());
+        ArrayList<Bruker> res = new ArrayList<>();
+
+        for (Bruker bruker : brukerList) {
+            res.add(bruker);
+        }
+        return res;
+    }
+
+    /**
+     * Sletter bruker med gitt epost.
+     *
+     * @param epost eposten til den brukeren som skal slettes fra databasen
+     * @return true hvis en eller flere rader fra tabellen har blitt slettet. false hvis ingen rader blir slettet.
+     */
+    public boolean slettBruker(String epost) {
+        if (epost == null)
+            return false;
+        JdbcTemplate con = new JdbcTemplate(dataKilde);
+        int num = con.update(slettBrukerSQL);
+
+        return num > 0;
+
     }
 }
