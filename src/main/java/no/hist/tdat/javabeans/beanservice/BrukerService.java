48e19b53d66fd98514d3d4c16790853c1e6901c2
eirikodal <eirik8830@hotmail.com>
2014-01-15 09:27:22 +0100

    start

---
+++ src/main/java/no/hist/tdat/javabeans/beanservice/BrukerService.java
@@ -0,0 +1,69 @@
+package no.hist.tdat.javabeans.beanservice;
+
+import no.hist.tdat.database.DatabaseConnector;
+import no.hist.tdat.javabeans.Bruker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class BrukerService {
+
+    @Autowired
+    DatabaseConnector databaseConnector;
+
+    /**
+     * Legger til en bruker i databasen
+     *
+     * @param bruker den brukeren du skal legge til i databasen
+     * @see
+     *
+     */
+    public boolean leggTilBruker(Bruker bruker) {
+        return databaseConnector.leggTilBruker(bruker);
+    }
+
+    /**
+     * Sjekker om mail og passord korresponderer
+     * Brukerobjekt opprettes av mail og passord før denne kalles.
+     *
+     * @param bruker brukeren som skal logges inn
+     * @return boolean, true om mail og passord korresponderer
+     */
+    public Bruker loggInn(Bruker bruker) {
+        return databaseConnector.loggInn(bruker);
+    }
+
+    /**
+     * Henter en bruker ut fra databasen
+     *
+     * @param email brukerens epost
+     * @return et brukerobjekt basert på databasedata funnet i databasen. Returnerer null hvis ingenting blir funnet.
+     */
+    public Bruker hentBruker(String email){
+        ArrayList<Bruker> brukere = databaseConnector.finnBruker(email);
+        return brukere.get(0);
+    }
+
+    /**
+     * Henter en liste med mulige bruker
+     * @param input søkeordet
+     * @return ArrayList med bruker objecter, eller null om ingen treffer med søkeordet
+     */
+    public ArrayList<Bruker> finnBruker(String input){
+        return databaseConnector.finnBruker(input);
+    }
+
+    /**
+     * Sletter en spesifikk bruker
+     * @param bruker brukerens objekt
+     * @return true om bruker ble slettet, ellers false
+     */
+    public boolean slettBruker (Bruker bruker){
+        return databaseConnector.slettBruker(bruker.getMail());
+    }
+
+
+
+}
---
+++ src/main/java/no/hist/tdat/kontrollere/LeggTilBrukerKontroller.java
@@ -0,0 +1,39 @@
+package no.hist.tdat.kontrollere;
+
+import no.hist.tdat.javabeans.Bruker;
+import no.hist.tdat.javabeans.beanservice.BrukerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+
+
+@Controller
+public class LeggTilBrukerKontroller {
+
+    @Autowired
+    BrukerService service;
+
+    /**
+     * Kontroller for å legge til bruker i databasen
+     * @param bruker
+     * @param result
+     * @return adminBrukere sin side, med feilmeldinger om
+     */
+    @RequestMapping("/leggTilBruker.htm")
+    public String leggTilBruker(@Valid @ModelAttribute("bruker") Bruker bruker, BindingResult result) {
+        if(result.hasErrors()){
+            System.out.println("Binding result feil");
+            return "adminBrukere";
+        }else{
+            if(service.leggTilBruker(bruker)){
+                return "adminBrukere";
+            }else{
+                return "adminBrukere";
+            }
+        }
+    }
+}
