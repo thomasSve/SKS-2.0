2b31e476f9d0b5d2734780dc0b16d1e66a436742
Thomas Astad Sve <thomas.asve@gmail.com>
2014-01-23 10:26:08 +0100

    fikset , i koeGrupper

--- src/main/java/no/hist/tdat/javabeans/KoeGrupper.java
+++ src/main/java/no/hist/tdat/javabeans/KoeGrupper.java
@@ -122,7 +122,12 @@ public class KoeGrupper {
     public String getOvingerIString(){
         String output="";
         for (int i = 0; i <ovinger.size() ; i++) {
-            output += ovinger.get(i).getOvingnr() + ", ";
+            if(i==0){
+                output += ovinger.get(i).getOvingnr();
+
+            }else{
+                output += ", " + ovinger.get(i).getOvingnr();
+            }
         }
         return output;
     }
